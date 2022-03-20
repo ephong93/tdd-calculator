0ea1c20be7e2056a332dce594756f42e2b5b8d1f
ephong93 <ephong93@gmail.com>
2022-03-20 19:32:56 +0900

    Refactor: getPanel function

--- src/CalculatorApp.test.js
+++ src/CalculatorApp.test.js
@@ -1,6 +1,14 @@
 import CalculatorApp from './CalculatorApp'
 import { render, screen, fireEvent } from '@testing-library/react'
 
+const getButton = (value) => {
+  return screen.getByRole('button', { name: value })
+}
+
+const getPanel = () => {
+  return screen.getByTestId('panel')
+}
+
 describe('CalculatorApp', () => {
   it('renders all buttons properly', () => {
     render(<CalculatorApp />)
@@ -16,8 +24,8 @@ describe('CalculatorApp', () => {
   })
   it('renders display', () => {
     render(<CalculatorApp />)
-    expect(screen.getByTestId('panel')).toBeTruthy()
-    expect(screen.getByTestId('panel').textContent).toBe('0')
+    expect(getPanel()).toBeTruthy()
+    expect(getPanel().textContent).toBe('0')
   })
 })
 
@@ -26,25 +34,25 @@ describe('update to zero', () => {
     render(<CalculatorApp />)
     const numberButton = screen.getByRole('button', { name: '1'})
     fireEvent.click(numberButton)
-    expect(screen.getByTestId('panel').textContent).toBe('1')
+    expect(getPanel().textContent).toBe('1')
   })
   
   it('does not update value when initially clicking 0', () => {
     render(<CalculatorApp />)
     const numberButton0 = screen.getByRole('button', { name: '0'})
     fireEvent.click(numberButton0)
-    expect(screen.getByTestId('panel').textContent).toBe('0')
+    expect(getPanel().textContent).toBe('0')
     fireEvent.click(numberButton0)
-    expect(screen.getByTestId('panel').textContent).toBe('0')
+    expect(getPanel().textContent).toBe('0')
   })
   it('adds sign properly', () => {
     render(<CalculatorApp />)
     const plusSignButton = screen.getByRole('button', { name: '+' })
     fireEvent.click(plusSignButton)
-    expect(screen.getByTestId('panel').textContent).toBe('0')
+    expect(getPanel().textContent).toBe('0')
     const minusSignButton = screen.getByRole('button', { name: '-' })
     fireEvent.click(minusSignButton)
-    expect(screen.getByTestId('panel').textContent).toBe('-0')
+    expect(getPanel().textContent).toBe('-0')
   })
   it('append to negative zero', () => {
     render(<CalculatorApp />)
@@ -52,8 +60,7 @@ describe('update to zero', () => {
     fireEvent.click(minusSignButton)
     const numberButton1 = screen.getByRole('button', { name: '1' })
     fireEvent.click(numberButton1)
-    expect(screen.getByTestId('panel').textContent).toBe('-1')
-
+    expect(getPanel().textContent).toBe('-1')
   })
 })
 
@@ -64,7 +71,7 @@ describe('append number', () => {
     fireEvent.click(numberButton1)
     const numberButton2 = screen.getByRole('button', { name: '2' })
     fireEvent.click(numberButton2)
-    expect(screen.getByTestId('panel').textContent).toBe('12')
+    expect(getPanel().textContent).toBe('12')
   })
   it('appends value to negative number in panel when clicking number buttons sequentially', () => {
     render(<CalculatorApp />)
@@ -73,6 +80,6 @@ describe('append number', () => {
     const numberButton1 = screen.getByRole('button', { name: '1' })
     fireEvent.click(numberButton1)
     fireEvent.click(numberButton1)
-    expect(screen.getByTestId('panel').textContent).toBe('-11')
+    expect(getPanel().textContent).toBe('-11')
   })
 })
