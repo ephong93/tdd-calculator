7edd16067a503d34a915b0d985fa10a46395648d
ephong93 <ephong93@gmail.com>
2022-03-20 14:07:53 +0900

    Refactor: use array to render number buttons

--- src/CalculatorForm.js
+++ src/CalculatorForm.js
@@ -1,18 +1,13 @@
 const CalculatorForm = ({
   onAppend
 }) => {
+  const numberArray = []
+  for (let i = 0; i <= 9; i++) {
+    numberArray.push(i.toString())
+  }
   return (
     <div>
-      <button data-testid='number-button' onClick={() => onAppend('0')}>0</button>
-      <button data-testid='number-button' onClick={() => onAppend('1')}>1</button>
-      <button data-testid='number-button' onClick={() => onAppend('2')}>2</button>
-      <button data-testid='number-button' onClick={() => onAppend('3')}>3</button>
-      <button data-testid='number-button' onClick={() => onAppend('4')}>4</button>
-      <button data-testid='number-button' onClick={() => onAppend('5')}>5</button>
-      <button data-testid='number-button' onClick={() => onAppend('6')}>6</button>
-      <button data-testid='number-button' onClick={() => onAppend('7')}>7</button>
-      <button data-testid='number-button' onClick={() => onAppend('8')}>8</button>
-      <button data-testid='number-button' onClick={() => onAppend('9')}>9</button>
+      { numberArray.map(number => <button data-testid='number-button' key={number} onClick={() => onAppend(number)}>{number}</button>) })
       <button data-testid='operation-button' onClick={() => onAppend('+')}>+</button>
       <button data-testid='operation-button' onClick={() => onAppend('-')}>-</button>
       <button data-testid='operation-button' onClick={() => onAppend('*')}>*</button>
