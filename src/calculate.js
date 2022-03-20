5bffa0fc968c32f4e8b5fbee5d448a1fe16cf5f4
ephong93 <ephong93@gmail.com>
2022-03-20 21:00:16 +0900

    Optimize using useCallback

---
+++ src/calculate.js
@@ -0,0 +1,61 @@
+const isNumber = (aValue) => {
+  return !isNaN(aValue)
+}
+
+const isOperation = (aValue) => {
+  return ['+', '-', '*', '/'].includes(aValue)
+}
+
+const removeSuffixOperations = (aValue) => {
+  if (isOperation(aValue[aValue.length - 1])) return removeSuffixOperations(aValue.substring(0, aValue.length - 1))
+  return aValue
+}
+  
+const calculateMulDiv = (aValue) => {
+  for (let i = aValue.length - 1; i >= 0; i--) {
+    if (aValue[i] === '*') {
+      return calculateMulDiv(aValue.substring(0, i)) * calculateMulDiv(aValue.substring(i+1, aValue.length))
+    }
+    if (aValue[i] === '/') {
+      const a = calculateMulDiv(aValue.substring(0, i))
+      const b = calculateMulDiv(aValue.substring(i+1, aValue.length))
+      if (b === 0) throw Error('Divided by zero!')
+      return a / b
+    }
+  }
+  return parseFloat(aValue)
+}
+
+const calculate = (aValue) => {
+  aValue = removeSuffixOperations(aValue)
+  for (let i = aValue.length - 1; i >= 1; i--) {
+    if (aValue[i] === '+') {
+      return calculate(aValue.substring(0, i)) + calculate(aValue.substring(i+1, aValue.length))
+    }
+    if (aValue[i] === '-') {
+      return calculate(aValue.substring(0, i)) - calculate(aValue.substring(i+1, aValue.length))
+    }
+  }
+  return calculateMulDiv(aValue)
+}
+
+const update = (a, b) => {
+  if (a === '0') {
+    if (b === '+') return '0'
+    if (b === '-') return '-0'
+    if (isNumber(b)) return b
+    return b
+  }
+  if (a === '-0') {
+    if (isNumber(b)) return '-' + b
+  }
+  if (a.length >= 2 && a[a.length - 1] === '0' && isOperation(a[a.length - 2]) && isNumber(b)) {
+    return a.substring(0, a.length - 1) + b
+  }
+  if (isOperation(a[a.length - 1]) && isOperation(b)) {
+    return a.substring(0, a.length - 1) + b
+  }
+  return a + b
+}
+
+export { calculate, update}
